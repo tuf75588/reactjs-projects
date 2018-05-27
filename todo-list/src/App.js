224a5cb97683b5d83b12f57c3bf9cd4e3d607ebe
Andrew Davis <tuf75588@temple.edu>
2018-05-26 22:15:08 -0400

    add eslint

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,53 +1,69 @@
 import React, { Component } from "react";
 
-
 class App extends Component {
   state = {
     message: `Hello Coding Garden`,
-    newTodo: '',
-  todos: [{
-    title: 'Learn React',
-    done: false
-  },
-    {
-      title: 'Learn JSX',
-      done: false,
-    }],
+    newTodo: "",
+    todos: [
+      {
+        title: "Learn React",
+        done: false
+      },
+      {
+        title: "Learn JSX",
+        done: false
+      }
+    ]
   };
-formSubmitted = (e) => {
-  e.preventDefault()
-  this.setState((prevState) => ({
-    todos: [...this.state.todos, {
-      title: this.state.newTodo,
-      done: false
-    }],
-    newTodo: ''
-  }))
-
-}
-toggleTodoDone = (e) => {
-  console.log(e.target)
-  this.setState({ done: !this.state.todos.done})
-}
-inputChange = (e) => {
-  const text = e.target.value;
-  this.setState({ newTodo: text })
-}
+  formSubmitted = e => {
+    e.preventDefault();
+    this.setState(prevState => ({
+      todos: [
+        ...this.state.todos,
+        {
+          title: this.state.newTodo,
+          done: false
+        }
+      ],
+      newTodo: ""
+    }));
+  };
+  toggleTodoDone = e => {
+    console.log(e.target);
+    this.setState({ done: !this.state.todos.done });
+  };
+  inputChange = e => {
+    const text = e.target.value;
+    this.setState({ newTodo: text });
+  };
   render() {
     return (
       <div className="App">
         <h3>{this.state.message}</h3>
         <form action="" onSubmit={this.formSubmitted}>
           <label htmlFor="newTodo">Add Todo</label>
-          <input type="text" id="newTodo" name="newTodo" onChange={this.inputChange} value={this.state.newTodo} />
+          <input
+            type="text"
+            id="newTodo"
+            name="newTodo"
+            onChange={this.inputChange}
+            value={this.state.newTodo}
+          />
           <button type="submit">Submit Todo</button>
         </form>
-        <ul>{this.state.todos.map(todo => {
-          return ( <li key={todo.title}>
-              <input type="checkbox" onChange={this.toggleTodoDone} checked={this.state.todos.done}/> {todo.title}
+        <ul>
+          {this.state.todos.map(todo => {
+            return (
+              <li key={todo.title}>
+                <input
+                  type="checkbox"
+                  onChange={this.toggleTodoDone}
+                  checked={this.state.todos.done}
+                />{" "}
+                {todo.title}
               </li>
-          )
-        })}
+            );
+          })}
         </ul>
       </div>
     );
