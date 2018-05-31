73fc91c59a0f25249e7d6aeff9ae4591c0a12077
Andrew Davis <tuf75588@temple.edu>
2018-05-30 22:32:36 -0400

    add component state to popular component

--- github-battle/app/index.js
+++ github-battle/app/index.js
@@ -1,12 +1,9 @@
 var React = require("react");
 var ReactDOM = require("react-dom");
 require("./index.css");
+var App = require('./components/App.js');
 
 
 
-class App extends React.Component {
-  render() {
-    return <div>Github Battle</div>;
-  }
-}
+
 ReactDOM.render(<App />, document.getElementById('app'));
