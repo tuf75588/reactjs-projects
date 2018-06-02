8f39e9830ae094b70365fbef8b4ca8de05feece2
Andrew Davis <tuf75588@temple.edu>
2018-06-02 00:53:36 -0400

    configure webpack

---
+++ react-weather-app/app/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import ReactDOM from 'react-dom'
+import './index.css'
+class App extends Component {
+  render() {
+    return <div>hello world</div>;
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("app"));
