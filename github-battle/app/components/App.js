888c8404fd8d017607b3b1d57649ba99def4af80
Andrew Davis <tuf75588@temple.edu>
2018-06-05 23:00:57 -0400

    add react router

--- github-battle/app/components/App.js
+++ github-battle/app/components/App.js
@@ -1,5 +1,8 @@
 var React = require("react");
 var Popular = require("./Popular");
+var ReactRouter = require("react-router-dom");
+var Link = require("react-router-dom").Link;
+var Router = require("react-router-dom").BrowserRouter;
 class App extends React.Component {
   render() {
     return (
