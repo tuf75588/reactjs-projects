68e0da2577f0430fba8dcb00121581f9dd0edc17
Andrew Davis <tuf75588@temple.edu>
2018-06-27 16:27:24 -0400

    add tank heros to render function

--- overwatch-home/src/App.js
+++ overwatch-home/src/App.js
@@ -1,12 +1,32 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { tankHeroes, damageHeroes, supportHeroes } from "./utils/heroData";
+import "./index.css";
 
-
+function HeroGrid(props) {
+  return (
+    <ul className="hero-container">
+      {props.tanks.map((hero, index) => {
+        return (
+          <li key={hero.name}>
+            <div className="hero">{hero.name}</div>
+          </li>
+        );
+      })}
+  
+    </ul>
+  );
+}
 class App extends Component {
+  state = {
+    isSelected: false
+  };
   render() {
     return (
-      <div className="App">
-      the world could always use more heroes
-        
+      <div className="container">
+        <div className="header">
+          <h1 className="title">Overwatch Hero Gallery</h1>
+        </div>
+      <HeroGrid tanks={tankHeroes} />
       </div>
     );
   }
--- overwatch-home/src/utils/heroData.js
+++ overwatch-home/src/utils/heroData.js
@@ -1,20 +1,177 @@
-const heros = [
-    {
-        name: 'Doomfist',
-        category: 'Damage',
-        description: '',
-        release: ''
-    },
-    {
-        name: 'Genji',
-        category: 'Damage',
-        description: '',
-        release: ''
-    },
-    {
-        name: 'McCree',
-        category: 'Damage',
-        description: '',
+const tankHeroes = [
+  {
+    name: "Dva",
+    release: "",
+    description: "",
+    abilities: []
+  },
+  {
+    name: "Orisa",
+    release: "",
+    description: "",
+    abilities: []
+  },
+  {
+    name: "Reinhardt",
+    release: "",
+    description: "",
+    abilities: []
+  },
+  {
+    name: "Roadhog",
+    release: "",
+    description: "",
+    abilities: []
+  },
+  {
+    name: "Winston",
+    release: "",
+    description: "",
+    abilities: []
+  },
+  {
+    name: "Zarya",
+    release: "",
+    description: "",
+    abilities: []
+  }
+];
+const damageHeroes = [
+  {
+    name: "Bastion",
+    release: "",
+    description: "",
+    abilities: []
+  },
+  {
+    name: "Doomfist",
+    release: "",
+    description: "",
+    abilities: []
+  },
+  {
+    name: "Genji",
+    release: "",
+    description: "",
+    abilities: []
+  },
+  {
+    name: "Hanzo",
+    release: "",
+    description: "",
+    abilities: []
+  },
+  {
+    name: "Junkrat",
+    release: "",
+    description: "",
+    abilities: []
+  },
+  {
+    name: "McCree",
+    release: "",
+    description: "",
+    abilities: []
+  },
+  {
+    name: "Mei",
+    release: "",
+    description: "",
+    abilities: []
+  },
+  {
+    name: "Pharah",
+    release: "",
+    description: "",
+    abilities: []
+  },
+  {
+    name: "Reaper",
+    release: "",
+    description: "",
+    abilities: []
+  },
+  {
+    name: "Soldier 76",
+    release: "",
+    description: "",
+    abilities: []
+  },
+  {
+    name: "Sombra",
+    release: "",
+    description: "",
+    abilities: []
+  },
+  {
+    name: "Symmetra",
+    release: "",
+    description: "",
+    abilities: []
+  },
+  {
+    name: "Torbjorn",
+    release: "",
+    description: "",
+    abilities: []
+  },
+  {
+    name: "Tracer",
+    release: "",
+    description: "",
+    abilities: []
+  },
+  {
+    name: "Widowmaker",
+    release: "",
+    description: "",
+    abilities: []
+  }
+];
 
-    }
-]
+
+const supportHeroes = [
+
+
+  {
+    name: "Ana",
+    release: "",
+    description: "",
+    abilities: []
+  },
+  {
+    name: "Brigitte",
+    release: "",
+    description: "",
+    abilities: []
+  },
+  {
+    name: "Lucio",
+    release: "",
+    description: "",
+    abilities: []
+  },
+  {
+    name: "Mercy",
+    release: "",
+    description: "",
+    abilities: []
+  },
+  {
+    name: "Moira",
+    release: "",
+    description: "",
+    abilities: []
+  },
+  {
+    name: "Zenyatta",
+    release: "",
+    description: "",
+    abilities: []
+  }
+];
+module.exports = {
+    tankHeroes,
+    damageHeroes,
+    supportHeroes
+}
