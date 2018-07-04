b987e26b439b4be2e49bc6d40afd4b1bd7174d86
Andrew Davis <tuf75588@temple.edu>
2018-07-03 23:12:58 -0400

    build out static ui

--- time_tracking_todo_app/public/js/app.js
+++ time_tracking_todo_app/public/js/app.js
@@ -1,2 +1,97 @@
 
 //build out static version of app
+
+class TimerDashboard extends React.Component {
+    render() {
+        return (
+            <div className="ui three column centered grid">
+                <div className="column">
+                    <EditableTimerList 
+                    isOpen={true}
+                    />
+                </div>
+            </div>
+        )
+    }
+}
+class EditableTimerList extends React.Component {
+    render() {
+        return (
+            <div id="timers">
+                <EditableTimer 
+                title='Learn React'
+                project='Web Domination'
+                elapsed='8986300'
+                runningSince={null}
+                editFormOpen={false}
+                />
+                <EditableTimer 
+                title='Learn Extreme Ironing'
+                project='World Domination'
+                elapsed='3890985'
+                runningSince={null}
+                editFormOpen={true}
+                />
+            </div>
+        )
+    }
+}
+class EditableTimer extends React.Component {
+    render() {
+        if(this.props.editFormOpen) {
+            return (
+                <TimerForm 
+                title={this.props.title}
+                project={this.props.project}
+                />
+            )
+        } else {
+            return (
+                <Timer 
+                title={this.props.title}
+                project={this.props.project}
+                elapsed={this.props.elapsed}
+                runningSince={this.props.runningSince}
+                />
+            )
+        }
+    }
+}
+class TimerForm extends React.Component {
+    render() {
+        return (
+            <div className="ui centered card">
+                <div className="content">
+                    <div className="ui form">
+                        <div className="field">
+                            <label htmlFor="">Title</label>
+                            <input type="text" defaultValue={this.props.title}/>
+                        </div>
+                        <div className="field">
+                            <label htmlFor="">Project</label>
+                            <input type="text" defaultValue={this.props.project}/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+class ToggableTimerForm extends React.Component {
+    render() {
+        if (this.props.isOpen) {
+            return (
+                <TimerForm />
+            )
+        } else {
+            return (
+                <div className="ui basic content center aligned segment">
+                    <button className="ui basic button icon">
+                        <i className="plus icon"></i>
+                    </button>
+                </div>
+            )
+        }
+    }
+} 
+ReactDOM.render(<TimerDashboard />, document.getElementById('main'))
