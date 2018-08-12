ad72958db775f55db8d5dd3944d78aac6da72121
Andrew Davis <tuf75588@temple.edu>
2018-08-11 22:09:02 -0400

    init

---
+++ wikipedia-search/src/App.js
@@ -0,0 +1,122 @@
+import React, { Component } from 'react';
+import styled, { css } from 'react-emotion';
+const _baseURL =
+  'https://en.wikipedia.org/w/api.php?action=opensearch&origin=*&format=json';
+
+const HeaderSection = styled('header')`
+  padding: 4em;
+  background-color: papayawhip;
+
+  margin-top: 50px;
+`;
+const Title = styled('h1')`
+  font-size: 2em;
+  text-align: center;
+  color: palevioletred;
+  font-weight: 400;
+`;
+
+const Header = props => (
+  <HeaderSection>
+    <Title>Wikipedia Search</Title>
+  </HeaderSection>
+);
+
+class App extends Component {
+  state = {
+    data: [],
+    searchTerm: '',
+    count: 3,
+    tableDisplay: 'none'
+  };
+  handleRangeChange = e => {
+    const val = parseInt(e.target.value);
+    this.setState(() => ({
+      count: val
+    }));
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+    return fetch(
+      `${_baseURL}&search=${this.state.searchTerm}&limit=${this.state.count}`
+    )
+      .then(data => {
+        return data.json();
+      })
+      .then(response => {
+        this.setState(() => ({
+          data: response
+        }));
+      });
+  };
+  handleInputChange = e => {
+    const val = e.target.value;
+    this.setState(() => ({
+      searchTerm: val
+    }));
+  };
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <div className="container">
+          <h3>Search Wikipeidia</h3>
+          <form onSubmit={this.handleSubmit}>
+            <input
+              type="text"
+              className="inputBox"
+              placeholder="Search Term.."
+              onChange={this.handleInputChange}
+              value={this.state.searchTerm}
+            />
+            <button type="submit" className="submitBtn">
+              Search
+            </button>
+          </form>
+
+          <button type="submit" className="randomBtn">
+            Or Generate a random article
+          </button>
+          <div className="rangeSection">
+            <input
+              min="3"
+              max="50"
+              type="range"
+              onChange={this.handleRangeChange}
+            />
+            <span className="counter">count: {this.state.count}</span>
+          </div>
+        </div>
+        <ResultsSection
+          data={this.state.data}
+          display={this.state.tableDisplay}
+          count={this.state.count}
+        />
+      </div>
+    );
+  }
+}
+
+function ResultsSection(props) {
+  const wikis = props.data.slice(0, props.count);
+  const titles = wikis[1];
+  const snippets = wikis[2];
+  const urls = props.data[3];
+  console.log(titles, snippets, urls);
+  const tableHeadStyle = {
+    display: props.tableDisplay
+  };
+
+  return (
+    <div className="resultsContainer">
+      {' '}
+      <table style={tableHeadStyle}>
+        <tr className="resultHead">
+          <th className="titleHead">Article Title</th>
+          <th className="infoHead">Snippet (click to view article)</th>
+        </tr>
+      </table>
+    </div>
+  );
+}
+export default App;
