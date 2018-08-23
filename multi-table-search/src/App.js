0d5b060a2e231b47a83204c47ab109d6184abe8b
Andrew Davis <tuf75588@temple.edu>
2018-08-23 18:08:40 -0400

    refactor

--- multi-table-search/src/App.js
+++ multi-table-search/src/App.js
@@ -1,73 +1,37 @@
 import React, { Component } from "react";
-import Header from "./components/Header";
-import SearchFields from "./components/SearchFields";
-import { getData } from "./utils/Api";
 
 class App extends Component {
   state = {
-    data: []
+    data: [],
+    country: "",
+    capital: "",
+    region: "",
+    subregion: ""
   };
   componentDidMount() {
-    getData().then(data => {
-      this.setState(() => ({
-        data
-      }));
-    });
+    this.getLocations();
   }
-  handleInputSearch = searchTerm => {
-    const original = [...this.state.data];
-    const { data } = this.state;
-    if (searchTerm.length > 0) {
-      const check = data.filter((element, index, array) => {
-        const regex = new RegExp(searchTerm, "gi");
-        const check = element.name.official.match(regex);
-        return check;
+  getLocations = () => {
+    const API_URL =
+      "https://raw.githubusercontent.com/mledoze/countries/master/countries.json";
+    return fetch(API_URL)
+      .then(res => res.json())
+      .then(result => {
+        console.log(result);
+        let data = {};
+        this.setState(() => ({
+          data: result.map((element, index, array) => {
+            data.name = element.name.official;
+            data.capital = element.capital[0];
+            data.region = element.region;
+            data.subregion = element.subregion;
+            return data;
+          })
+        }));
       });
-      this.setState(() => ({
-        data: check
-      }));
-    }
   };
   render() {
-    const tableFileds = this.state.data.map((element, index) => {
-      const capital = element.capital[0];
-      const officialName = element.name.official;
-      const region = element.region;
-      const subregion = element.subregion;
-      const lat = element.latlng[0];
-      const lng = element.latlng[1];
-      return (
-        <tr className="data" key={index}>
-          <td>{officialName}</td>
-          <td>{capital}</td>
-          <td>{region}</td>
-          <td>{subregion}</td>
-          <td>{lat}</td>
-          <td>{lng}</td>
-        </tr>
-      );
-    });
-    return (
-      <div className="container">
-        <Header title="Country/Capital Data Multi-Search Service" />
-        <SearchFields handleInputSearch={this.handleInputSearch} />
-        <div className="tableContainer">
-          <table className="tableData">
-            <thead>
-              <tr>
-                <th>Country</th>
-                <th>Capital</th>
-                <th>Region</th>
-                <th>Subregion</th>
-                <th>Latitude</th>
-                <th>Longitude</th>
-              </tr>
-            </thead>
-            {tableFileds}
-          </table>
-        </div>
-      </div>
-    );
+    return <div>hi</div>;
   }
 }
 
